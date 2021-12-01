e7fafed0860aa4820564c5061f8a07eeb709113d
droid333 <73542834+droid333@users.noreply.github.com>
2021-12-01 18:35:57 +0100

    Add files via upload

---
+++ c_02/ft_strcapitalize.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slucas <slucas@student.42mulhouse>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 12:43:08 by slucas            #+#    #+#             */
+/*   Updated: 2021/11/17 14:55:55 by slucas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	ft_tst(char c, char a, char b)
+{
+	if (c >= a && c <= b)
+		return (1);
+	else
+		return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+	char	s;
+
+	i = 0;
+	//s = str[i - 1];
+	ft_strupcase(str);
+	while (str[i] != '\0')
+	{
+		s = str[i - 1];
+		if (ft_tst(s, '0', '9') || ft_tst(s, 'A', 'Z') || ft_tst(s, 'a', 'z'))
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] += 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main(void)
+{
+    char    str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+    //char    str[] = "";
+    ft_strcapitalize(str);
+    printf("%s", ft_strcapitalize(str));
+    return (0);
+}
