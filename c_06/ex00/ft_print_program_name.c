524df4b7ae3210d49ae4dd0c38614bcd7908697e
droid333 <73542834+droid333@users.noreply.github.com>
2021-12-01 18:37:06 +0100

    Add files via upload

---
+++ c_06/ex00/ft_print_program_name.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slucas <slucas@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 15:07:20 by slucas            #+#    #+#             */
+/*   Updated: 2021/11/24 17:38:04 by slucas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		ft_putstr(argv[0]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
---
+++ c_06/ex02/ft_rev_params.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slucas <slucas@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 18:49:37 by slucas            #+#    #+#             */
+/*   Updated: 2021/11/24 15:59:39 by slucas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc > 0)
+	{
+		i = argc - 1;
+		while (i > 0)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i--;
+		}
+	}
+	return (0);
+}
